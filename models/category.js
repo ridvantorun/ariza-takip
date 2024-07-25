2f045443b27e71b07e896e7c89421a3ea98fddc3
ridvantorun <rdvan.torunn@gmail.com>
2024-07-25 16:44:48 +0300

    added category editing function

--- models/category.js
+++ models/category.js
@@ -2,16 +2,22 @@ const getDb = require('../utility/database').getdb;
 const mongodb = require('mongodb');
 
 class Category {
-    constructor(name, description) {
+    constructor(name, description, id) {
         this.name = name;
         this.description = description;
+        this._id = id ? new mongodb.ObjectId(id) : null;
     }
 
     save() {
-        const db = getDb();
+        let db = getDb();
 
-        return db.collection('categories')
-            .insertOne(this)
+        if (this._id) {
+            db = db.collection('categories').updateOne({ _id: this._id }, { $set: this });
+        } else {
+            db = db.collection('categories').insertOne(this);
+        }
+
+        return db
             .then(result => console.log(result))
             .catch(err => console.log(err));
     }
@@ -25,6 +31,16 @@ class Category {
                 return categories;
             })
             .catch(err => console.log(err));
+    }
+
+    static findById(categoryid) {
+        const db = getDb();
+
+        return db.collection('categories')
+            .findOne({ _id: new mongodb.ObjectId(categoryid) })
+            .then(category => {
+                return category;
+            }).catch(err => console.log(err));
     }
 
 
